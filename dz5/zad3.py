9e8d15f2d9fa9a5a24063cdbead03583d95f70aa
george <georgercls@gmail.com>
2022-10-11 14:08:26 +0300

    6 seminardz

--- dz5/zad3.py
+++ dz5/zad3.py
@@ -1,38 +1,42 @@
-# Создайте программу для игры в 'Крестики-нолики'.
-position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-victories = [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-win = ""
-def get_result():
-    
- 
-    for i in range(len(victories)):
-        if victories[i] == "X":
-            win = "X"
-        if victories[i] == "O":
-            win = "O"   
-             
-    return win
- 
+from random import randint
 
-sign = "0"
-while True:
-    if sign == "0":
-       sign = "X"
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+value = int(input("Введите количество конфет на столе: "))
+flag = randint(0,2)
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
     else:
-        sign = "0"
-    
-    print(f'{position[0]:^5}|{position[1]:^5}|{position[2]:^5}')
-    print("-------------------")
-    print(f'{position[3]:^5}|{position[4]:^5}|{position[5]:^5}')
-    print("-------------------")
-    print(f'{position[6]:^5}|{position[7]:^5}|{position[8]:^5}')
-    
-    index = int(input("\n\nСделайте ход: "))
-    
-    position[index - 1] = sign
-    if win != "":
-        game_over = 'победили X'
-    else:
-        game_over = 'победили 0'
-    print(game_over)
-    
+        k = input_dat(player2)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
---
+++ dz6/zad2.py
@@ -0,0 +1,5 @@
+# Задание с перемешиванием списка можно высвести через шафл, так удобнее
+import random 
+spisok = [1, 2, 3, 4, 5]
+random.shuffle(spisok)
+print(spisok)
